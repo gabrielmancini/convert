1d1eae279a4d3c504096c5e2c5fbf6259feb9d49
Gabriel Mancini <gabriel.mancini@gmail.com>
2018-01-17 10:02:58 -0200

    create stream for transform data

--- test/cdn.test.js
+++ test/cdn.test.js
@@ -1,17 +1,29 @@
 const fs = require('fs')
 const assert = require('assert')
 const convert = require('../')
+var LineStream = require('readline-stream')
 describe('cdn suite', function () {
-  var minhacdn
+  var minhaCdn
   var agora
   var lMinhaCdn = '312|200|HIT|"GET /robots.txt HTTP/1.1"|100.2'
   var lAgora = '"MINHA CDN" GET 200 /robots.txt 100 312 HIT'
   before(function () {
-    minhacdn = fs.readFileSync('./test/fixtures/minhacdn.txt').toString()
+    minhaCdn = fs.createReadStream('./test/fixtures/minhacdn.txt')
     agora = fs.readFileSync('./test/fixtures/agora.txt').toString()
   })
-  it('shold be able convert content', function () {
-    assert.equal(convert(lMinhaCdn), lAgora)
+  it('shold be able convert content', function (done) {
+    var bytes = ''
+    minhaCdn
+      .pipe(LineStream({}))
+      .pipe(convert.convert())
+      .on('data', function (chunk) {
+        bytes += chunk
+      })
+    .on('finish', function () {
+      console.log('err', arguments)
+      assert.equal(bytes, agora)
+      done()
+    })
   })
   it('shold be able to get the url content', function () {
 
